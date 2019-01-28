c4970d1129cfb32f4bc842b6a23182c25276698a
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-01-28 16:32:46 +0100

    Actually trigger the gradle release

--- scripts/travis-release.sh
+++ scripts/travis-release.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -ev
-git tag pushback2
 git remote set-url origin https://${github_pushback_personal_token}@github.com/${TRAVIS_REPO_SLUG}
-git push origin pushback2
+./gradlew release
