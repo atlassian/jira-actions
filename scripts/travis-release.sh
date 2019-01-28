91583f7197564fafe3b0f7bcf9843744c493731b
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-01-28 17:31:56 +0100

    Disable the Axcion "ahead of remote check"

--- scripts/travis-release.sh
+++ scripts/travis-release.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 set -ev
-git status
-git branch ${TRAVIS_BRANCH} -u origin/${TRAVIS_BRANCH}
-git status
-./gradlew release -Prelease.customUsername=${github_pushback_personal_token}
+./gradlew release -Prelease.customUsername=${github_pushback_personal_token} -Prelease.disableRemoteCheck
