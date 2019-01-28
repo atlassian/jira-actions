6abde9038a7ab2b0cfd5e57812c4899da57a6d8b
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-01-28 15:52:21 +0100

    Mutate the Git origin in order for gradle-release plugin to pick up the GH token

--- scripts/pushback.sh
+++ scripts/pushback.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -ev
-git tag pushback
-git push https://${github_pushback_personal_token}@github.com/dagguh/jira-actions.git pushback
+git tag pushback2
+git remote set-url origin https://${github_pushback_personal_token}@github.com/${TRAVIS_REPO_SLUG}
+git push origin pushback2
