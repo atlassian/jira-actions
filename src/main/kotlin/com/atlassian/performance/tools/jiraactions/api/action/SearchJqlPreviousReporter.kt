02df0a7b5acb47d63dde16e5174cb123960c5337
dramotowski <dramotowski@atlassian.com>
2019-07-17 13:35:32 +0200

    JPERF-528: New action implemented 'Search JQL by previous reporter'.

---
+++ src/main/kotlin/com/atlassian/performance/tools/jiraactions/api/action/SearchJqlPreviousReporter.kt
@@ -0,0 +1,38 @@
+package com.atlassian.performance.tools.jiraactions.api.action
+
+import com.atlassian.performance.tools.jiraactions.api.WebJira
+import com.atlassian.performance.tools.jiraactions.api.measure.ActionMeter
+import com.atlassian.performance.tools.jiraactions.api.memories.IssueKeyMemory
+import com.atlassian.performance.tools.jiraactions.api.memories.JqlMemory
+import com.atlassian.performance.tools.jiraactions.api.observation.SearchJqlObservation
+import com.atlassian.performance.tools.jiraactions.api.page.IssueNavigatorPage
+import com.atlassian.performance.tools.jiraactions.jql.BuiltInJQL
+import com.atlassian.performance.tools.jiraactions.api.SEARCH_WITH_JQL_PREV_REPORTER
+import java.util.function.Predicate
+import javax.json.JsonObject
+
+class SearchJqlPreviousReporter(
+    private val jira: WebJira,
+    private val meter: ActionMeter,
+    private val jqlMemory: JqlMemory,
+    private val issueKeyMemory: IssueKeyMemory
+) : Action {
+
+    override fun run() {
+        val jqlQuery = jqlMemory.recallByTag(Predicate { it == BuiltInJQL.REPORTERS.name })!!
+        val issueNavigatorPage = meter.measure(
+            key = SEARCH_WITH_JQL_PREV_REPORTER,
+            action = { jira.goToIssueNavigator(jqlQuery).waitForIssueNavigator() },
+            observation = this::observe
+        )
+        issueKeyMemory.remember(issueNavigatorPage.getIssueKeys())
+    }
+
+    private fun observe(
+        page: IssueNavigatorPage
+    ): JsonObject {
+        val issueKeys = page.getIssueKeys()
+        issueKeyMemory.remember(issueKeys)
+        return SearchJqlObservation(page.jql, issueKeys.size, page.getTotalResults()).serialize()
+    }
+}
