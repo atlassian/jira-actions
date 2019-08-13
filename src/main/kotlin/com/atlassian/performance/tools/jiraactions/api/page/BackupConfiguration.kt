056842fc7c0bf78542fff5daecc0f8844779c26a
Pawel Lipinski <plipinski@atlassian.com>
2019-08-13 13:23:25 +0200

    JPERF-344: Add disable backup service feature

---
+++ src/main/kotlin/com/atlassian/performance/tools/jiraactions/api/page/BackupConfiguration.kt
@@ -0,0 +1,42 @@
+package com.atlassian.performance.tools.jiraactions.api.page
+
+import org.apache.logging.log4j.LogManager
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+
+class BackupConfiguration(
+    private val driver: WebDriver,
+    private val access: AdminAccess
+) {
+    private val logger = LogManager.getLogger(this::class.java)
+    private val deleteBackupLocator = By.xpath(
+        "//*[@id='tbl_services']" +
+            "//*[contains(text(), 'com.atlassian.jira.service.services.export.ExportService')]" +
+            "/ancestor::tr" +
+            "//*[contains(@class, 'operations-list')]" +
+            "//*[contains(text(), 'Delete')]"
+    )
+
+    fun delete(): BackupConfiguration {
+        val prompted = access.isPrompted()
+        if (prompted) {
+            access.gain()
+        }
+        while (driver.isElementPresent(deleteBackupLocator)) {
+            deleteBackupService()
+        }
+        logger.info("Backup services have been deleted")
+        if (prompted) {
+            access.drop()
+        }
+        return this
+    }
+
+    private fun deleteBackupService() {
+        driver.findElement(deleteBackupLocator).click()
+        driver.switchTo().alert().accept()
+        if (access.isPrompted()) {
+            access.gain()
+        }
+    }
+}
