2817dade70c75531bc883770f83b4852281f842e
Przemek Bruski <pbruski@atlassian.com>
2020-01-31 02:09:35 +0100

    click. click again. click better.

--- src/main/kotlin/com/atlassian/performance/tools/jiraactions/page/SingleSelect.kt
+++ src/main/kotlin/com/atlassian/performance/tools/jiraactions/page/SingleSelect.kt
@@ -1,6 +1,8 @@
 package com.atlassian.performance.tools.jiraactions.page
 
+import com.atlassian.performance.seleniumjs.LocatorConverters
 import com.atlassian.performance.tools.jiraactions.api.page.wait
+import com.atlassian.performance.tools.jiraactions.api.webdriver.JavaScriptUtils
 import org.openqa.selenium.By
 import org.openqa.selenium.Keys
 import org.openqa.selenium.WebDriver
@@ -11,9 +13,9 @@ import java.time.Duration
 
 internal class SingleSelect(
     private val driver: WebDriver,
-    element: By
+    private val locator: By
 ) {
-    private val parent: By = ByChained(element, By.xpath(".."))
+    private val parent: By = ByChained(locator, By.xpath(".."))
     private val input = ByChained(parent, By.tagName("input"))
 
     fun select(value: String) {
@@ -21,13 +23,24 @@ internal class SingleSelect(
             timeout = Duration.ofSeconds(8),
             condition = elementToBeClickable(input)
         )
-        inputElement.clear()
-        // this is a workaround for the fact that clear() can be processed asynchronously and can remove
-        // some of the keys sent to the browser. We have tested that under stress load, sending one backspace
-        // before the value fixes this issue.
-        // Whether we lose the backspace or not, it doesn't matter for the overall result.
+        //unfortunately, there are race conditions in the single select, so 'just typing' won't work
+        //there are also race conditions around .clear()
+        //we have to manually remove the content of the field with backspaces
         inputElement.click()
-        inputElement.sendKeys(Keys.BACK_SPACE, value, Keys.ENTER)
+        val valueLength = getLength(locator)
+        inputElement.sendKeys(Keys.BACK_SPACE.repeat(valueLength), value, Keys.ENTER)
+    }
+
+    private fun getLength(locator: By): Int {
+        //note: inputElement.text will return empty text at this point, we need to use JavaScript :-(
+        val nativeLocator = try {
+            LocatorConverters.toNativeBy(locator)
+        } catch (e: Exception) {
+            // conversion failed, let's hope that sending one backspace will be enough.
+            return 1
+        }
+        val inputElementText = JavaScriptUtils.executeScript<String>(driver, "return " + nativeLocator.render() + ".value")
+        return inputElementText.length
     }
 
     fun getSuggestions(): List<String> {
