8c8539d0485e70c989b809280d162f2de365766d
Przemek Bruski <pbruski@atlassian.com>
2020-02-03 16:11:49 +0100

    shortcuts in admin access

--- src/main/kotlin/com/atlassian/performance/tools/jiraactions/api/page/AdminAccess.kt
+++ src/main/kotlin/com/atlassian/performance/tools/jiraactions/api/page/AdminAccess.kt
@@ -11,7 +11,7 @@ class AdminAccess(
     private val jira: WebJira,
     private val password: String
 ) {
-    private val dropLocator = By.cssSelector(
+    private val dropAdminRightsLocator = By.cssSelector(
         "#websudo-drop-from-protected-page, #websudo-drop-from-normal-page"
     )
     private val passwordLocator = By.id(
@@ -19,12 +19,18 @@ class AdminAccess(
     )
 
     fun isPrompted(): Boolean {
+        if (isDropNotificationPresent()) {
+            return false
+        }
         return Patience().test {
             driver.isElementPresent(passwordLocator)
         }
     }
 
     fun isGranted(): Boolean {
+        if (isDropNotificationPresent()) {
+            return true
+        }
         jira.goToSystemInfo()
         val granted = isPrompted().not()
         driver.navigate().back()
@@ -49,9 +55,14 @@ class AdminAccess(
     }
 
     fun drop() {
-        jira.goToDashboard().waitForDashboard()
-        if (isGranted()) {
-            driver.wait(elementToBeClickable(dropLocator)).click()
+        if (!isDropNotificationPresent()) {
+            jira.goToDashboard().waitForDashboard()
+            if (!isGranted()) {
+                return
+            }
         }
+        driver.wait(elementToBeClickable(dropAdminRightsLocator)).click()
     }
+
+    private fun isDropNotificationPresent() = driver.findElements(dropAdminRightsLocator).isNotEmpty()
 }
