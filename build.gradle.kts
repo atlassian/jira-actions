atlassian-labs/aws-resources

commit 6e272c387f3dd60b44a0d4dfd019ae98b559a6a8

Upgraded AWS SDK to 1.11.817

build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation("com.atlassian.performance.tools:concurrency:[1.0.0,2.0.0)")
     implementation("com.atlassian.performance.tools:io:[1.2.0,2.0.0)")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jre8:$kotlinVersion")
-    implementation("commons-codec:commons-codec:1.10")
+    implementation("commons-codec:commons-codec:1.11")
     aws(
         "ec2",
         "s3",
@@ -58,7 +58,7 @@ fun aws(
     vararg modules: String
 ): List<String> = modules
     .map { module ->
-        "com.amazonaws:aws-java-sdk-$module:1.11.424"
+        "com.amazonaws:aws-java-sdk-$module:1.11.817"
     }
     .plus(log4j("jcl"))
     .plus(jaxb())

gradle/dependency-locks/apiDependenciesMetadata.lockfile
@@ -1,37 +1,37 @@
 # This is a Gradle generated file for dependency locking.
 # Manual edits can break the build and are not advised.
 # This file is expected to be part of source control.
-com.amazonaws:aws-java-sdk-cloudformation:1.11.424
-com.amazonaws:aws-java-sdk-core:1.11.424
-com.amazonaws:aws-java-sdk-ec2:1.11.424
-com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.424
-com.amazonaws:aws-java-sdk-iam:1.11.424
-com.amazonaws:aws-java-sdk-kms:1.11.424
-com.amazonaws:aws-java-sdk-rds:1.11.424
-com.amazonaws:aws-java-sdk-s3:1.11.424
-com.amazonaws:aws-java-sdk-sts:1.11.424
-com.amazonaws:aws-java-sdk-support:1.11.424
-com.amazonaws:jmespath-java:1.11.424
+com.amazonaws:aws-java-sdk-cloudformation:1.11.817
+com.amazonaws:aws-java-sdk-core:1.11.817
+com.amazonaws:aws-java-sdk-ec2:1.11.817
+com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.817
+com.amazonaws:aws-java-sdk-iam:1.11.817
+com.amazonaws:aws-java-sdk-kms:1.11.817
+com.amazonaws:aws-java-sdk-rds:1.11.817
+com.amazonaws:aws-java-sdk-s3:1.11.817
+com.amazonaws:aws-java-sdk-sts:1.11.817
+com.amazonaws:aws-java-sdk-support:1.11.817
+com.amazonaws:jmespath-java:1.11.817
 com.atlassian.performance.tools:io:1.2.0
 com.atlassian.performance.tools:jvm-tasks:1.0.0
 com.atlassian.performance.tools:ssh:2.1.0
 com.fasterxml.jackson.core:jackson-annotations:2.6.0
 com.fasterxml.jackson.core:jackson-core:2.6.7
-com.fasterxml.jackson.core:jackson-databind:2.6.7.1
+com.fasterxml.jackson.core:jackson-databind:2.6.7.3
 com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.6.7
 com.github.stephenc.jcip:jcip-annotations:1.0-1
 com.hierynomus:sshj:0.23.0
 com.jcraft:jzlib:1.1.3
 com.sun.istack:istack-commons-runtime:3.0.5
 com.sun.xml.fastinfoset:FastInfoset:1.2.13
-commons-codec:commons-codec:1.10
+commons-codec:commons-codec:1.11
 commons-logging:commons-logging:1.2
 javax.activation:activation:1.1.1
 javax.xml.bind:jaxb-api:2.3.0
 joda-time:joda-time:2.8.1
 net.i2p.crypto:eddsa:0.2.0
-org.apache.httpcomponents:httpclient:4.5.5
-org.apache.httpcomponents:httpcore:4.4.9
+org.apache.httpcomponents:httpclient:4.5.9
+org.apache.httpcomponents:httpcore:4.4.11
 org.apache.logging.log4j:log4j-api:2.10.0
 org.apache.logging.log4j:log4j-core:2.10.0
 org.apache.logging.log4j:log4j-jcl:2.10.0

gradle/dependency-locks/compileClasspath.lockfile
@@ -1,38 +1,38 @@
 # This is a Gradle generated file for dependency locking.
 # Manual edits can break the build and are not advised.
 # This file is expected to be part of source control.
-com.amazonaws:aws-java-sdk-cloudformation:1.11.424
-com.amazonaws:aws-java-sdk-core:1.11.424
-com.amazonaws:aws-java-sdk-ec2:1.11.424
-com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.424
-com.amazonaws:aws-java-sdk-iam:1.11.424
-com.amazonaws:aws-java-sdk-kms:1.11.424
-com.amazonaws:aws-java-sdk-rds:1.11.424
-com.amazonaws:aws-java-sdk-s3:1.11.424
-com.amazonaws:aws-java-sdk-sts:1.11.424
-com.amazonaws:aws-java-sdk-support:1.11.424
-com.amazonaws:jmespath-java:1.11.424
+com.amazonaws:aws-java-sdk-cloudformation:1.11.817
+com.amazonaws:aws-java-sdk-core:1.11.817
+com.amazonaws:aws-java-sdk-ec2:1.11.817
+com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.817
+com.amazonaws:aws-java-sdk-iam:1.11.817
+com.amazonaws:aws-java-sdk-kms:1.11.817
+com.amazonaws:aws-java-sdk-rds:1.11.817
+com.amazonaws:aws-java-sdk-s3:1.11.817
+com.amazonaws:aws-java-sdk-sts:1.11.817
+com.amazonaws:aws-java-sdk-support:1.11.817
+com.amazonaws:jmespath-java:1.11.817
 com.atlassian.performance.tools:concurrency:1.0.0
 com.atlassian.performance.tools:io:1.2.0
 com.atlassian.performance.tools:jvm-tasks:1.0.0
 com.atlassian.performance.tools:ssh:2.1.0
 com.fasterxml.jackson.core:jackson-annotations:2.6.0
 com.fasterxml.jackson.core:jackson-core:2.6.7
-com.fasterxml.jackson.core:jackson-databind:2.6.7.1
+com.fasterxml.jackson.core:jackson-databind:2.6.7.3
 com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.6.7
 com.github.stephenc.jcip:jcip-annotations:1.0-1
 com.hierynomus:sshj:0.23.0
 com.jcraft:jzlib:1.1.3
 com.sun.istack:istack-commons-runtime:3.0.5
 com.sun.xml.fastinfoset:FastInfoset:1.2.13
-commons-codec:commons-codec:1.10
+commons-codec:commons-codec:1.11
 commons-logging:commons-logging:1.2
 javax.activation:activation:1.1.1
 javax.xml.bind:jaxb-api:2.3.0
 joda-time:joda-time:2.8.1
 net.i2p.crypto:eddsa:0.2.0
-org.apache.httpcomponents:httpclient:4.5.5
-org.apache.httpcomponents:httpcore:4.4.9
+org.apache.httpcomponents:httpclient:4.5.9
+org.apache.httpcomponents:httpcore:4.4.11
 org.apache.logging.log4j:log4j-api:2.10.0
 org.apache.logging.log4j:log4j-core:2.10.0
 org.apache.logging.log4j:log4j-jcl:2.10.0

gradle/dependency-locks/default.lockfile
@@ -1,38 +1,38 @@
 # This is a Gradle generated file for dependency locking.
 # Manual edits can break the build and are not advised.
 # This file is expected to be part of source control.
-com.amazonaws:aws-java-sdk-cloudformation:1.11.424
-com.amazonaws:aws-java-sdk-core:1.11.424
-com.amazonaws:aws-java-sdk-ec2:1.11.424
-com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.424
-com.amazonaws:aws-java-sdk-iam:1.11.424
-com.amazonaws:aws-java-sdk-kms:1.11.424
-com.amazonaws:aws-java-sdk-rds:1.11.424
-com.amazonaws:aws-java-sdk-s3:1.11.424
-com.amazonaws:aws-java-sdk-sts:1.11.424
-com.amazonaws:aws-java-sdk-support:1.11.424
-com.amazonaws:jmespath-java:1.11.424
+com.amazonaws:aws-java-sdk-cloudformation:1.11.817
+com.amazonaws:aws-java-sdk-core:1.11.817
+com.amazonaws:aws-java-sdk-ec2:1.11.817
+com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.817
+com.amazonaws:aws-java-sdk-iam:1.11.817
+com.amazonaws:aws-java-sdk-kms:1.11.817
+com.amazonaws:aws-java-sdk-rds:1.11.817
+com.amazonaws:aws-java-sdk-s3:1.11.817
+com.amazonaws:aws-java-sdk-sts:1.11.817
+com.amazonaws:aws-java-sdk-support:1.11.817
+com.amazonaws:jmespath-java:1.11.817
 com.atlassian.performance.tools:concurrency:1.0.0
 com.atlassian.performance.tools:io:1.2.0
 com.atlassian.performance.tools:jvm-tasks:1.0.0
 com.atlassian.performance.tools:ssh:2.1.0
 com.fasterxml.jackson.core:jackson-annotations:2.6.0
 com.fasterxml.jackson.core:jackson-core:2.6.7
-com.fasterxml.jackson.core:jackson-databind:2.6.7.1
+com.fasterxml.jackson.core:jackson-databind:2.6.7.3
 com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.6.7
 com.github.stephenc.jcip:jcip-annotations:1.0-1
 com.hierynomus:sshj:0.23.0
 com.jcraft:jzlib:1.1.3
 com.sun.istack:istack-commons-runtime:3.0.5
 com.sun.xml.fastinfoset:FastInfoset:1.2.13
-commons-codec:commons-codec:1.10
+commons-codec:commons-codec:1.11
 commons-logging:commons-logging:1.2
 javax.activation:activation:1.1.1
 javax.xml.bind:jaxb-api:2.3.0
 joda-time:joda-time:2.8.1
 net.i2p.crypto:eddsa:0.2.0
-org.apache.httpcomponents:httpclient:4.5.5
-org.apache.httpcomponents:httpcore:4.4.9
+org.apache.httpcomponents:httpclient:4.5.9
+org.apache.httpcomponents:httpcore:4.4.11
 org.apache.logging.log4j:log4j-api:2.10.0
 org.apache.logging.log4j:log4j-core:2.10.0
 org.apache.logging.log4j:log4j-jcl:2.10.0

gradle/dependency-locks/implementationDependenciesMetadata.lockfile
@@ -1,38 +1,38 @@
 # This is a Gradle generated file for dependency locking.
 # Manual edits can break the build and are not advised.
 # This file is expected to be part of source control.
-com.amazonaws:aws-java-sdk-cloudformation:1.11.424
-com.amazonaws:aws-java-sdk-core:1.11.424
-com.amazonaws:aws-java-sdk-ec2:1.11.424
-com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.424
-com.amazonaws:aws-java-sdk-iam:1.11.424
-com.amazonaws:aws-java-sdk-kms:1.11.424
-com.amazonaws:aws-java-sdk-rds:1.11.424
-com.amazonaws:aws-java-sdk-s3:1.11.424
-com.amazonaws:aws-java-sdk-sts:1.11.424
-com.amazonaws:aws-java-sdk-support:1.11.424
-com.amazonaws:jmespath-java:1.11.424
+com.amazonaws:aws-java-sdk-cloudformation:1.11.817
+com.amazonaws:aws-java-sdk-core:1.11.817
+com.amazonaws:aws-java-sdk-ec2:1.11.817
+com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.817
+com.amazonaws:aws-java-sdk-iam:1.11.817
+com.amazonaws:aws-java-sdk-kms:1.11.817
+com.amazonaws:aws-java-sdk-rds:1.11.817
+com.amazonaws:aws-java-sdk-s3:1.11.817
+com.amazonaws:aws-java-sdk-sts:1.11.817
+com.amazonaws:aws-java-sdk-support:1.11.817
+com.amazonaws:jmespath-java:1.11.817
 com.atlassian.performance.tools:concurrency:1.0.0
 com.atlassian.performance.tools:io:1.2.0
 com.atlassian.performance.tools:jvm-tasks:1.0.0
 com.atlassian.performance.tools:ssh:2.1.0
 com.fasterxml.jackson.core:jackson-annotations:2.6.0
 com.fasterxml.jackson.core:jackson-core:2.6.7
-com.fasterxml.jackson.core:jackson-databind:2.6.7.1
+com.fasterxml.jackson.core:jackson-databind:2.6.7.3
 com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.6.7
 com.github.stephenc.jcip:jcip-annotations:1.0-1
 com.hierynomus:sshj:0.23.0
 com.jcraft:jzlib:1.1.3
 com.sun.istack:istack-commons-runtime:3.0.5
 com.sun.xml.fastinfoset:FastInfoset:1.2.13
-commons-codec:commons-codec:1.10
+commons-codec:commons-codec:1.11
 commons-logging:commons-logging:1.2
 javax.activation:activation:1.1.1
 javax.xml.bind:jaxb-api:2.3.0
 joda-time:joda-time:2.8.1
 net.i2p.crypto:eddsa:0.2.0
-org.apache.httpcomponents:httpclient:4.5.5
-org.apache.httpcomponents:httpcore:4.4.9
+org.apache.httpcomponents:httpclient:4.5.9
+org.apache.httpcomponents:httpcore:4.4.11
 org.apache.logging.log4j:log4j-api:2.10.0
 org.apache.logging.log4j:log4j-core:2.10.0
 org.apache.logging.log4j:log4j-jcl:2.10.0

gradle/dependency-locks/runtimeClasspath.lockfile
@@ -1,38 +1,38 @@
 # This is a Gradle generated file for dependency locking.
 # Manual edits can break the build and are not advised.
 # This file is expected to be part of source control.
-com.amazonaws:aws-java-sdk-cloudformation:1.11.424
-com.amazonaws:aws-java-sdk-core:1.11.424
-com.amazonaws:aws-java-sdk-ec2:1.11.424
-com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.424
-com.amazonaws:aws-java-sdk-iam:1.11.424
-com.amazonaws:aws-java-sdk-kms:1.11.424
-com.amazonaws:aws-java-sdk-rds:1.11.424
-com.amazonaws:aws-java-sdk-s3:1.11.424
-com.amazonaws:aws-java-sdk-sts:1.11.424
-com.amazonaws:aws-java-sdk-support:1.11.424
-com.amazonaws:jmespath-java:1.11.424
+com.amazonaws:aws-java-sdk-cloudformation:1.11.817
+com.amazonaws:aws-java-sdk-core:1.11.817
+com.amazonaws:aws-java-sdk-ec2:1.11.817
+com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.817
+com.amazonaws:aws-java-sdk-iam:1.11.817
+com.amazonaws:aws-java-sdk-kms:1.11.817
+com.amazonaws:aws-java-sdk-rds:1.11.817
+com.amazonaws:aws-java-sdk-s3:1.11.817
+com.amazonaws:aws-java-sdk-sts:1.11.817
+com.amazonaws:aws-java-sdk-support:1.11.817
+com.amazonaws:jmespath-java:1.11.817
 com.atlassian.performance.tools:concurrency:1.0.0
 com.atlassian.performance.tools:io:1.2.0
 com.atlassian.performance.tools:jvm-tasks:1.0.0
 com.atlassian.performance.tools:ssh:2.1.0
 com.fasterxml.jackson.core:jackson-annotations:2.6.0
 com.fasterxml.jackson.core:jackson-core:2.6.7
-com.fasterxml.jackson.core:jackson-databind:2.6.7.1
+com.fasterxml.jackson.core:jackson-databind:2.6.7.3
 com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.6.7
 com.github.stephenc.jcip:jcip-annotations:1.0-1
 com.hierynomus:sshj:0.23.0
 com.jcraft:jzlib:1.1.3
 com.sun.istack:istack-commons-runtime:3.0.5
 com.sun.xml.fastinfoset:FastInfoset:1.2.13
-commons-codec:commons-codec:1.10
+commons-codec:commons-codec:1.11
 commons-logging:commons-logging:1.2
 javax.activation:activation:1.1.1
 javax.xml.bind:jaxb-api:2.3.0
 joda-time:joda-time:2.8.1
 net.i2p.crypto:eddsa:0.2.0
-org.apache.httpcomponents:httpclient:4.5.5
-org.apache.httpcomponents:httpcore:4.4.9
+org.apache.httpcomponents:httpclient:4.5.9
+org.apache.httpcomponents:httpcore:4.4.11
 org.apache.logging.log4j:log4j-api:2.10.0
 org.apache.logging.log4j:log4j-core:2.10.0
 org.apache.logging.log4j:log4j-jcl:2.10.0

gradle/dependency-locks/testCompileClasspath.lockfile
@@ -1,39 +1,39 @@
 # This is a Gradle generated file for dependency locking.
 # Manual edits can break the build and are not advised.
 # This file is expected to be part of source control.
-com.amazonaws:aws-java-sdk-cloudformation:1.11.424
-com.amazonaws:aws-java-sdk-core:1.11.424
-com.amazonaws:aws-java-sdk-ec2:1.11.424
-com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.424
-com.amazonaws:aws-java-sdk-iam:1.11.424
-com.amazonaws:aws-java-sdk-kms:1.11.424
-com.amazonaws:aws-java-sdk-rds:1.11.424
-com.amazonaws:aws-java-sdk-s3:1.11.424
-com.amazonaws:aws-java-sdk-sts:1.11.424
-com.amazonaws:aws-java-sdk-support:1.11.424
-com.amazonaws:jmespath-java:1.11.424
+com.amazonaws:aws-java-sdk-cloudformation:1.11.817
+com.amazonaws:aws-java-sdk-core:1.11.817
+com.amazonaws:aws-java-sdk-ec2:1.11.817
+com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.817
+com.amazonaws:aws-java-sdk-iam:1.11.817
+com.amazonaws:aws-java-sdk-kms:1.11.817
+com.amazonaws:aws-java-sdk-rds:1.11.817
+com.amazonaws:aws-java-sdk-s3:1.11.817
+com.amazonaws:aws-java-sdk-sts:1.11.817
+com.amazonaws:aws-java-sdk-support:1.11.817
+com.amazonaws:jmespath-java:1.11.817
 com.atlassian.performance.tools:concurrency:1.0.0
 com.atlassian.performance.tools:io:1.2.0
 com.atlassian.performance.tools:jvm-tasks:1.0.0
 com.atlassian.performance.tools:ssh:2.1.0
 com.fasterxml.jackson.core:jackson-annotations:2.6.0
 com.fasterxml.jackson.core:jackson-core:2.6.7
-com.fasterxml.jackson.core:jackson-databind:2.6.7.1
+com.fasterxml.jackson.core:jackson-databind:2.6.7.3
 com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.6.7
 com.github.stephenc.jcip:jcip-annotations:1.0-1
 com.hierynomus:sshj:0.23.0
 com.jcraft:jzlib:1.1.3
 com.sun.istack:istack-commons-runtime:3.0.5
 com.sun.xml.fastinfoset:FastInfoset:1.2.13
-commons-codec:commons-codec:1.10
+commons-codec:commons-codec:1.11
 commons-logging:commons-logging:1.2
 javax.activation:activation:1.1.1
 javax.xml.bind:jaxb-api:2.3.0
 joda-time:joda-time:2.8.1
 junit:junit:4.12
 net.i2p.crypto:eddsa:0.2.0
-org.apache.httpcomponents:httpclient:4.5.5
-org.apache.httpcomponents:httpcore:4.4.9
+org.apache.httpcomponents:httpclient:4.5.9
+org.apache.httpcomponents:httpcore:4.4.11
 org.apache.logging.log4j:log4j-api:2.10.0
 org.apache.logging.log4j:log4j-core:2.10.0
 org.apache.logging.log4j:log4j-jcl:2.10.0

gradle/dependency-locks/testImplementationDependenciesMetadata.lockfile
@@ -1,39 +1,39 @@
 # This is a Gradle generated file for dependency locking.
 # Manual edits can break the build and are not advised.
 # This file is expected to be part of source control.
-com.amazonaws:aws-java-sdk-cloudformation:1.11.424
-com.amazonaws:aws-java-sdk-core:1.11.424
-com.amazonaws:aws-java-sdk-ec2:1.11.424
-com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.424
-com.amazonaws:aws-java-sdk-iam:1.11.424
-com.amazonaws:aws-java-sdk-kms:1.11.424
-com.amazonaws:aws-java-sdk-rds:1.11.424
-com.amazonaws:aws-java-sdk-s3:1.11.424
-com.amazonaws:aws-java-sdk-sts:1.11.424
-com.amazonaws:aws-java-sdk-support:1.11.424
-com.amazonaws:jmespath-java:1.11.424
+com.amazonaws:aws-java-sdk-cloudformation:1.11.817
+com.amazonaws:aws-java-sdk-core:1.11.817
+com.amazonaws:aws-java-sdk-ec2:1.11.817
+com.amazonaws:aws-java-sdk-elasticloadbalancing:1.11.817
+com.amazonaws:aws-java-sdk-iam:1.11.817
+com.amazonaws:aws-java-sdk-kms:1.11.817
+com.amazonaws:aws-java-sdk-rds:1.11.817
+com.amazonaws:aws-java-sdk-s3:1.11.817
+com.amazonaws:aws-java-sdk-sts:1.11.817
+com.amazonaws:aws-java-sdk-support:1.11.817
+com.amazonaws:jmespath-java:1.11.817
 com.atlassian.performance.tools:concurrency:1.0.0
 com.atlassian.performance.tools:io:1.2.0
 com.atlassian.performance.tools:jvm-tasks:1.0.0
 com.atlassian.performance.tools:ssh:2.1.0
 com.fasterxml.jackson.core:jackson-annotations:2.6.0
 com.fasterxml.jackson.core:jackson-core:2.6.7
-com.fasterxml.jackson.core:jackson-databind:2.6.7.1
+com.fasterxml.jackson.core:jackson-databind:2.6.7.3
 com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.6.7
 com.github.stephenc.jcip:jcip-annotations:1.0-1
 com.hierynomus:sshj:0.23.0
 com.jcraft:jzlib:1.1.3
 com.sun.istack:istack-commons-runtime:3.0.5
 com.sun.xml.fastinfoset:FastInfoset:1.2.13
-commons-codec:commons-codec:1.10
+commons-codec:commons-codec:1.11
 commons-logging:commons-logging:1.2
 javax.activation:activation:1.1.1
 javax.xml.bind:jaxb-api:2.3.0
 joda-time:joda-time:2.8.1
 junit:junit:4.12
 net.i2p.crypto:eddsa:0.2.0
-org.apache.httpcomponents:httpclient:4.5.5
-org.apache.httpcomponents:httpcore:4.4.9
+org.apache.httpcomponents:httpclient:4.5.9
+org.apache.httpcomponents:httpcore:4.4.11
 org.apache.logging.log4j:log4j-api:2.10.0
 org.apache.logging.log4j:log4j-core:2.10.0
 org.apache.logging.log4j:log4j-jcl:2.10.0

atlassian/jira-actions

commit 7732940287bfe6fb7b86c43b40cf6c46b6c4f878

JPERF-730: Bump commons-codec, so that the module is compatible with recent versions of aws-resources (1.7.x)
Compatibility with aws-resources was broken with https://bitbucket.org/atlassian/aws-resources/commits/6e272c387f3dd60b44a0d4dfd019ae98b559a6a8 (version 1.6.1)

build.gradle.kts
@@ -22,7 +22,7 @@ configurations.all {
         failOnVersionConflict()
         eachDependency {
             when (requested.module.toString()) {
-                "commons-codec:commons-codec" -> useVersion("1.10")
+                "commons-codec:commons-codec" -> useVersion("1.11")
                 "org.jetbrains:annotations" -> useVersion("13.0")
                 "org.slf4j:slf4j-api" -> useVersion("1.7.25")
                 "org.testcontainers:testcontainers" -> useVersion("1.15.1")