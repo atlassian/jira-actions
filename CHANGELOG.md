a709d32dede73efdf9ad5f10fc4fcaaadf6bf8f2
Ewelina Fiedorowicz <efiedorowicz@atlassian.com>
2023-03-22 08:10:27 +0100

    JPERF-1043 Add support for list view on IssueNavigatorPage. Add IssueNavigatorView enum class that represents both available layouts, use it as an optional parameter in SearchJqlSimpleAction and SearchJqlSimpleAction constructors. When no value provided then detail view will be used as a default layout.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -26,6 +26,9 @@ Dropping a requirement of a major version of a dependency is a new contract.
 ## [Unreleased]
 [Unreleased]: https://github.com/atlassian/jira-actions/compare/release-3.18.1...master
 
+### Added
+- Add optional `IssueNavigatorView` parameter to JQL actions constructors. Make `IssueNavigationPage` support both list view and split view. Fix [JPERF-1043]. 
+
 ## [3.18.1] - 2023-02-02
 [3.18.1]: https://github.com/atlassian/jira-actions/compare/release-3.18.0...release-3.18.1
 
--- src/main/kotlin/com/atlassian/performance/tools/jiraactions/api/page/IssueNavigatorPage.kt
+++ src/main/kotlin/com/atlassian/performance/tools/jiraactions/api/page/IssueNavigatorPage.kt
@@ -6,40 +6,63 @@ import com.atlassian.performance.seleniumjs.NativeExpectedConditions.Companion.p
 import com.atlassian.performance.tools.jiraactions.api.webdriver.JavaScriptUtils
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable
 import java.time.Duration
 
 open class IssueNavigatorPage(
     private val driver: WebDriver,
     val jql: String
 ) {
+    private val detailView = and(
+        or(
+            presenceOfElementLocated(By.cssSelector("ol.issue-list")),
+            presenceOfElementLocated(By.id("issuetable")),
+            presenceOfElementLocated(By.id("issue-content"))
+        ),
+        presenceOfElementLocated(By.id("key-val")),
+        presenceOfElementLocated(By.className("issue-body-content"))
+    )
+    private val listView = and(
+        presenceOfElementLocated(By.id("issuetable")),
+        presenceOfElementLocated(By.cssSelector(".issuerow.focused"))
+    )
+    private val emptyResults = presenceOfElementLocated(By.className("no-results-hint"))
 
-    /**
-     * Requires an issue detail pane to be visible.
-     * For example, if there are no issues found, this will time out.
-     */
     fun waitForIssueNavigator(): IssueNavigatorPage {
         val jiraErrors = JiraErrors(driver)
         driver.wait(
-            Duration.ofSeconds(30),
+            Duration.ofSeconds(10),
             or(
-                and(
-                    or(
-                        presenceOfElementLocated(By.cssSelector("ol.issue-list")),
-                        presenceOfElementLocated(By.id("issuetable")),
-                        presenceOfElementLocated(By.id("issue-content"))
-                    ),
-                    presenceOfElementLocated(By.id("key-val")),
-                    presenceOfElementLocated(By.className("issue-body-content"))
-                ),
-                presenceOfElementLocated(By.className("no-results-hint")),
+                detailView,
+                listView,
+                emptyResults,
                 jiraErrors.anyCommonErrorNative()
             )
         )
         return this
     }
 
+    fun switchLayoutTo(view: IssueNavigatorView): IssueNavigatorPage {
+        val currentLayout = getCurrentLayout()
+        if (currentLayout != view) {
+            driver.wait(elementToBeClickable(By.id("layout-switcher-button"))).click()
+            driver.wait(elementToBeClickable(By.cssSelector("a[data-layout-key=${view.selector}]"))).click()
+        }
+        return this
+    }
+
+    private fun getCurrentLayout(): IssueNavigatorView {
+        driver.wait(Duration.ofSeconds(10), presenceOfElementLocated(By.className("results-panel")))
+        return if (driver.isElementPresent(By.cssSelector("div.list-view"))) {
+            IssueNavigatorView.LIST_VIEW
+        } else {
+            IssueNavigatorView.DETAIL_VIEW
+        }
+    }
+
     open fun getIssueKeys(): Set<String> {
-        val issueKeys: List<String> = JavaScriptUtils.executeScript(driver,
+        val issueKeys: List<String> = JavaScriptUtils.executeScript(
+            driver,
             "return Array.from(document.getElementsByClassName('issue-link-key'), i => i.innerText.trim())"
         )
 
@@ -54,7 +77,7 @@ open class IssueNavigatorPage(
         return IssuePage(driver).getIssueId()
     }
 
-    protected fun getDriver() : WebDriver {
+    protected fun getDriver(): WebDriver {
         return this.driver
     }
 
@@ -65,4 +88,9 @@ open class IssueNavigatorPage(
         ?.trim()
         ?.substringAfter("of ")
         ?.toInt() ?: 0
+}
+
+enum class IssueNavigatorView(val selector: String) {
+    LIST_VIEW("list-view"),
+    DETAIL_VIEW("split-view")
 }
