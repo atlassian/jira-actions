7bc676cc359cd85f40986db1464baf41f0b39653
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-03-24 09:25:01 +0100

    JPERF-1043: Extract `IssueNavResultsView`
    Most of `IssueNavigatorPage` was assuming the detail view, including:
    * how to detect the search results
    * how to count results
    * how to extract issue keys

--- src/main/kotlin/com/atlassian/performance/tools/jiraactions/api/page/IssueNavigatorPage.kt
+++ src/main/kotlin/com/atlassian/performance/tools/jiraactions/api/page/IssueNavigatorPage.kt
@@ -1,14 +1,12 @@
 package com.atlassian.performance.tools.jiraactions.api.page
 
-import com.atlassian.performance.seleniumjs.NativeExpectedCondition
-import com.atlassian.performance.seleniumjs.NativeExpectedConditions.Companion.and
 import com.atlassian.performance.seleniumjs.NativeExpectedConditions.Companion.or
 import com.atlassian.performance.seleniumjs.NativeExpectedConditions.Companion.presenceOfElementLocated
-import com.atlassian.performance.tools.jiraactions.api.page.IssueNavigatorPage.View.*
-import com.atlassian.performance.tools.jiraactions.api.webdriver.JavaScriptUtils
+import com.atlassian.performance.tools.jiraactions.api.page.issuenav.DetailView
+import com.atlassian.performance.tools.jiraactions.api.page.issuenav.IssueNavResultsView
+import com.atlassian.performance.tools.jiraactions.page.issuenav.UnknownView
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
-import org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable
 import java.time.Duration
 
 open class IssueNavigatorPage(
@@ -17,13 +15,15 @@ open class IssueNavigatorPage(
 ) {
     private val emptyResults = presenceOfElementLocated(By.className("no-results-hint"))
 
-    fun waitForIssueNavigator(): IssueNavigatorPage {
+    @Deprecated("Waiting for results depend on the results view", ReplaceWith("waitForResults"))
+    fun waitForIssueNavigator(): IssueNavigatorPage = waitForResults(UnknownView(driver))
+
+    fun waitForResults(results: IssueNavResultsView): IssueNavigatorPage {
         val jiraErrors = JiraErrors(driver)
         driver.wait(
             Duration.ofSeconds(10),
             or(
-                DETAIL_VIEW.resultsPresent,
-                LIST_VIEW.resultsPresent,
+                results.detectResults(),
                 emptyResults,
                 jiraErrors.anyCommonErrorNative()
             )
@@ -31,32 +31,9 @@ open class IssueNavigatorPage(
         return this
     }
 
-    fun switchTo(view: View): IssueNavigatorPage {
-        val currentView = getCurrentView()
-        if (currentView != view) {
-            driver.wait(elementToBeClickable(By.id("layout-switcher-button"))).click()
-            driver.wait(elementToBeClickable(By.cssSelector("a[data-layout-key=${view.selector}]"))).click()
-        }
-        return this
-    }
 
-    private fun getCurrentView(): View {
-        driver.wait(Duration.ofSeconds(10), presenceOfElementLocated(By.className("results-panel")))
-        return if (driver.isElementPresent(By.cssSelector("div.list-view"))) {
-            LIST_VIEW
-        } else {
-            DETAIL_VIEW
-        }
-    }
-
-    open fun getIssueKeys(): Set<String> {
-        val issueKeys: List<String> = JavaScriptUtils.executeScript(
-            driver,
-            "return Array.from(document.getElementsByClassName('issue-link-key'), i => i.innerText.trim())"
-        )
-
-        return HashSet(issueKeys)
-    }
+    @Deprecated("Issue keys depend on the results view", ReplaceWith("IssueNavResultsView.listIssueKeys().toSet()"))
+    open fun getIssueKeys(): Set<String> = UnknownView(driver).listIssueKeys().toSet()
 
     fun issueView(): IssuePage {
         return IssuePage(driver)
@@ -70,36 +47,6 @@ open class IssueNavigatorPage(
         return this.driver
     }
 
-    fun getTotalResults(): Int = driver
-        .findElements(By.className("showing"))
-        .singleOrNull()
-        ?.text
-        ?.trim()
-        ?.substringAfter("of ")
-        ?.toInt() ?: 0
-
-    enum class View(
-        val selector: String,
-        val resultsPresent: NativeExpectedCondition
-    ) {
-        LIST_VIEW(
-            "list-view",
-            and(
-                presenceOfElementLocated(By.id("issuetable")),
-                presenceOfElementLocated(By.cssSelector(".issuerow.focused"))
-            )
-        ),
-        DETAIL_VIEW(
-            "split-view",
-            and(
-                or(
-                    presenceOfElementLocated(By.cssSelector("ol.issue-list")),
-                    presenceOfElementLocated(By.id("issuetable")),
-                    presenceOfElementLocated(By.id("issue-content"))
-                ),
-                presenceOfElementLocated(By.id("key-val")),
-                presenceOfElementLocated(By.className("issue-body-content"))
-            )
-        )
-    }
+    @Deprecated("Total results depend on the results view", ReplaceWith("IssueNavResultsView.countResults()"))
+    fun getTotalResults(): Int = UnknownView(driver).countResults() ?: 0
 }
