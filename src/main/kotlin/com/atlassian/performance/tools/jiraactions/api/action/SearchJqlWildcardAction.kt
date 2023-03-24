ed2af826771774488ac465555b693483f51f0c3a
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-03-24 16:39:29 +0100

    JPERF-1043: Coalesce JQL actions together
    The new action is better, because:
    * it supports `IssueNavResultsView`
    * doesn't copy-paste so much code
    * doesn't call `IssueKeyMemory.remember` twice
    * doesn't rely on smelly `JqlMemory.recallByTag` API
    * distinguishes counting "no issues found" and "unknown issues found"
    
    I call `recallByTag` smelly, because:
    * there's no such thing as "JQL tags" for the users
    * it's way too easy to think `Predicate<String>` filters JQLs, not tags
    * it's bound too tightly to `AdaptiveJqlMemory`
    
    But I'm not deprecating it here, because I'd need to produce a
    replacement, which basically means decoupling `AdaptiveJqlMemory`
    into "JQL baking" and "JQL filtering", but it's too much for this scope.
    
    PS. Restore binary compatibility of constructors of:
    * `SearchJqlAction`
    * `SearchJqlChangelogAction`
    * `SearchJqlSimpleAction`
    
    Adding Kotlin default parameters doesn't preserve the method signatures.

--- src/main/kotlin/com/atlassian/performance/tools/jiraactions/api/action/SearchJqlWildcardAction.kt
+++ src/main/kotlin/com/atlassian/performance/tools/jiraactions/api/action/SearchJqlWildcardAction.kt
@@ -1,47 +1,28 @@
 package com.atlassian.performance.tools.jiraactions.api.action
 
-import com.atlassian.performance.tools.jiraactions.api.SEARCH_JQL_CHANGELOG
 import com.atlassian.performance.tools.jiraactions.api.SEARCH_WITH_JQL_WILDCARD
+import com.atlassian.performance.tools.jiraactions.api.SeededRandom
 import com.atlassian.performance.tools.jiraactions.api.WebJira
 import com.atlassian.performance.tools.jiraactions.api.measure.ActionMeter
 import com.atlassian.performance.tools.jiraactions.api.memories.IssueKeyMemory
 import com.atlassian.performance.tools.jiraactions.api.memories.JqlMemory
-import com.atlassian.performance.tools.jiraactions.api.observation.SearchJqlObservation
-import com.atlassian.performance.tools.jiraactions.api.page.IssueNavigatorPage
 import com.atlassian.performance.tools.jiraactions.jql.BuiltInJQL
-import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger
+import com.atlassian.performance.tools.jiraactions.memories.jql.TagSelectiveJqlMemory
 import java.util.function.Predicate
-import javax.json.JsonObject
 
+@Deprecated("Use SearchIssues.Builder")
 class SearchJqlWildcardAction(
-    private val jira: WebJira,
-    private val meter: ActionMeter,
-    private val jqlMemory: JqlMemory,
-    private val issueKeyMemory: IssueKeyMemory
+    jira: WebJira,
+    meter: ActionMeter,
+    jqlMemory: JqlMemory,
+    issueKeyMemory: IssueKeyMemory
 ) : Action {
-    private val logger: Logger = LogManager.getLogger(this::class.java)
 
-    override fun run() {
-        val jqlQuery = jqlMemory.recallByTag(Predicate { it == BuiltInJQL.GENERIC_WIDE.name })
-        if (jqlQuery == null) {
-            logger.debug("Skipping ${SEARCH_WITH_JQL_WILDCARD.label} action. I have no knowledge of wildcard JQL queries.")
-            return
-        }
+    private val action = SearchIssues.Builder(jira, meter, SeededRandom())
+        .actionType(SEARCH_WITH_JQL_WILDCARD)
+        .jqlMemory(TagSelectiveJqlMemory(jqlMemory, Predicate { it == BuiltInJQL.GENERIC_WIDE.name }))
+        .issueKeyMemory(issueKeyMemory)
+        .build()
 
-        val issueNavigatorPage = meter.measure(
-            key = SEARCH_WITH_JQL_WILDCARD,
-            action = { jira.goToIssueNavigator(jqlQuery).waitForIssueNavigator() },
-            observation = this::observe
-        )
-        issueKeyMemory.remember(issueNavigatorPage.getIssueKeys())
-    }
-
-    private fun observe(
-        page: IssueNavigatorPage
-    ): JsonObject {
-        val issueKeys = page.getIssueKeys()
-        issueKeyMemory.remember(issueKeys)
-        return SearchJqlObservation(page.jql, issueKeys.size, page.getTotalResults()).serialize()
-    }
+    override fun run() = action.run()
 }
