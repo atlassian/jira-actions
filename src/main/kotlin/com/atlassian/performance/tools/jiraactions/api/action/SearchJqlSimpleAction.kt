2d4ab60d53655bd2ac33491eff5301e4ef20fadc
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-03-24 16:39:29 +0100

    JPERF-1043: Coalesce JQL actions together
    The new action is better, because:
    * it supports `IssueNavResultsView`
    * doesn't copy-paste so much code
    * doesn't call `IssueKeyMemory.remember` twice
    * doesn't rely on smelly `JqlMemory.recallByTag` API
    * distinguishes counting "no issues found" and "unknown issues found"
    
    I call `recallByTag` smelly, because:
    * there's no such thing as "JQL tags" for the users
    * it's way too easy to think `Predicate<String>` filters JQLs, not tags
    * it's bound too tightly to `AdaptiveJqlMemory`
    
    But I'm not deprecating it here, because I'd need to produce a
    replacement, which basically means decoupling `AdaptiveJqlMemory`
    into "JQL baking" and "JQL filtering", but it's too much for this scope.
    
    PS. Restore binary compatibility of constructors of:
    * `SearchJqlAction`
    * `SearchJqlChangelogAction`
    * `SearchJqlSimpleAction`
    
    Adding Kotlin default parameters doesn't preserve the method signatures.

--- src/main/kotlin/com/atlassian/performance/tools/jiraactions/api/action/SearchJqlSimpleAction.kt
+++ src/main/kotlin/com/atlassian/performance/tools/jiraactions/api/action/SearchJqlSimpleAction.kt
@@ -1,49 +1,32 @@
 package com.atlassian.performance.tools.jiraactions.api.action
 
 import com.atlassian.performance.tools.jiraactions.api.SEARCH_JQL_SIMPLE
+import com.atlassian.performance.tools.jiraactions.api.SeededRandom
 import com.atlassian.performance.tools.jiraactions.api.WebJira
 import com.atlassian.performance.tools.jiraactions.api.measure.ActionMeter
 import com.atlassian.performance.tools.jiraactions.api.memories.IssueKeyMemory
 import com.atlassian.performance.tools.jiraactions.api.memories.JqlMemory
-import com.atlassian.performance.tools.jiraactions.api.observation.SearchJqlObservation
-import com.atlassian.performance.tools.jiraactions.api.page.IssueNavigatorPage
-import com.atlassian.performance.tools.jiraactions.api.page.issuenav.DetailView
-import com.atlassian.performance.tools.jiraactions.api.page.issuenav.IssueNavResultsView
 import com.atlassian.performance.tools.jiraactions.jql.BuiltInJQL
-import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger
+import com.atlassian.performance.tools.jiraactions.memories.jql.TagSelectiveJqlMemory
 import java.util.function.Predicate
-import javax.json.JsonObject
 
+@Deprecated("Use SearchIssues.Builder")
 class SearchJqlSimpleAction(
-    private val jira: WebJira,
-    private val meter: ActionMeter,
-    private val jqlMemory: JqlMemory,
-    private val issueKeyMemory: IssueKeyMemory,
-    private val view: IssueNavResultsView = DetailView(jira.driver)
+    jira: WebJira,
+    meter: ActionMeter,
+    jqlMemory: JqlMemory,
+    issueKeyMemory: IssueKeyMemory
 ) : Action {
-    private val logger: Logger = LogManager.getLogger(this::class.java)
 
-    override fun run() {
-        val jqlQueries = jqlMemory.recallByTag(Predicate { it != BuiltInJQL.GENERIC_WIDE.name && it != BuiltInJQL.REPORTERS.name })
-        if (jqlQueries == null) {
-            logger.debug("Skipping ${SEARCH_JQL_SIMPLE.label} action. I have no knowledge of simple JQL queries.")
-            return
-        }
-
-        meter.measure(
-            key = SEARCH_JQL_SIMPLE,
-            action = { jira.goToIssueNavigator(jqlQueries).also { view.switchToView() }.waitForResults(view) },
-            observation = this::observe
-        )
-        issueKeyMemory.remember(view.listIssueKeys())
+    private val jqlTagFilter: Predicate<String> = Predicate {
+        it != BuiltInJQL.GENERIC_WIDE.name && it != BuiltInJQL.REPORTERS.name
     }
 
-    private fun observe(
-        page: IssueNavigatorPage
-    ): JsonObject {
-        val issueKeys = view.listIssueKeys()
-        issueKeyMemory.remember(issueKeys)
-        return SearchJqlObservation(page.jql, issueKeys.size, view.countResults() ?: 0).serialize()
-    }
+    private val action = SearchIssues.Builder(jira, meter, SeededRandom())
+        .actionType(SEARCH_JQL_SIMPLE)
+        .jqlMemory(TagSelectiveJqlMemory(jqlMemory, jqlTagFilter))
+        .issueKeyMemory(issueKeyMemory)
+        .build()
+
+    override fun run() = action.run()
 }
