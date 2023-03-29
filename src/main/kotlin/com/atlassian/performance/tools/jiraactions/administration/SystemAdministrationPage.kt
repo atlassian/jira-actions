ca78f5e78f656e9159f197b2e53905f75718dacc
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-03-29 15:34:32 +0200

    JPERF-1050: Recognize "Support Tools" in 7.2.0
    The element id has changed somewhere between 7.2.0 and 7.13.0.

--- src/main/kotlin/com/atlassian/performance/tools/jiraactions/administration/SystemAdministrationPage.kt
+++ src/main/kotlin/com/atlassian/performance/tools/jiraactions/administration/SystemAdministrationPage.kt
@@ -6,8 +6,19 @@ import org.openqa.selenium.WebDriver
 internal class SystemAdministrationPage(
     private val driver: WebDriver
 ) {
+
+    /**
+     * Element id, which works in 7.2.0.
+     */
+    private val supportToolsIdOld = "#support-tools-home"
+
+    /**
+     * Element id, which works in 7.13.0, 8.0.0 - 8.20.0 and 9.0.0.
+     */
+    private val supportToolsIdNew = "#troubleshooting-home"
+
     fun troubleshootingAndSupportTools(): TroubleShootingAndSupportToolsPage {
-        driver.findElement(By.id("troubleshooting-home")).click()
+        driver.findElement(By.cssSelector("$supportToolsIdOld,$supportToolsIdNew")).click()
         return TroubleShootingAndSupportToolsPage(driver)
     }
 }
